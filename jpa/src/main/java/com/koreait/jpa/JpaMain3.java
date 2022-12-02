108d40b67657c7596f1c325e9b44f4636f0f88aa
kdmyeong94 <115456038+kdmyeong94@users.noreply.github.com>
2022-12-02 23:40:28 +0900

    1202

---
+++ jpa/src/main/java/com/koreait/jpa/JpaMain3.java
@@ -0,0 +1,39 @@
+package com.koreait.jpa;
+
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.EntityTransaction;
+import javax.persistence.Persistence;
+
+public class JpaMain3 {
+
+	public static void main(String[] args) {
+			
+		EntityManagerFactory emf = Persistence.createEntityManagerFactory("hello");
+		EntityManager em = emf.createEntityManager();
+		
+		// transcation 발생
+		EntityTransaction tx = em.getTransaction();
+		tx.begin();
+		
+		try {
+			
+			Member2 member2 = new Member2();
+			
+//			member2.setId("ID_A");
+			member2.setUsername("Hello");
+			
+			em.persist(member2);
+			
+			tx.commit();
+		} catch (Exception e) {
+			tx.rollback();
+		} finally {
+			em.close();
+			emf.close();		
+		}
+		
+	}
+
+}
